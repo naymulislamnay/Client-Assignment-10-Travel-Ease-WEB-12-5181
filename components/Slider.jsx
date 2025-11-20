73f205900c567dd8fc1632d9482819d5b5daa6c8
naymulislamnay <naymulislamnay@gmail.com>
2025-11-20 19:17:12 +0600

    slider added

---
+++ components/Slider.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Link } from 'react-router';
+
+const Slider = ({ sliderVehicles }) => {
+    return (
+        <div className="my-8">
+            <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+                loop={true}
+                spaceBetween={30}
+                slidesPerView={1}
+                className="rounded-2xl"
+            >
+                {sliderVehicles.map((vehicle) => (
+                    <SwiperSlide key={vehicle._id}>
+                        <div className="relative">
+                            <img
+                                src={vehicle.coverImage}
+                                alt={vehicle.vehicleName}
+                                className="w-full h-[200px] md:h-[400px] object-cover rounded-2xl"
+                            />
+                            <div className="absolute inset-0 bg-linear-to-r from-[#000000] via-[#00000090] to-[#00000001] bg-opacity-40 flex items-center justify-center w-[75%]  lg:w-1/2 flex-col">
+                                <h2 className="text-white text-[16px] md:text-[20px] lg:text-2xl md:text-3xl font-bold ml-2.5">
+                                    {vehicle.vehicleName}
+                                </h2>
+                                <p className="text-white text-[10px] md:text-[12px] lg:text-[16px] w-3/4">
+                                    {vehicle.description}
+                                </p>
+                                <Link to={`/vehicles/${vehicle._id}`}>
+                                    <button className="btn bg-linear-to-br from-[#024c58] to-[#07b6d5] border-none text-[10px] md:text-[13px] lg:text-[16px] text-white mt-2.5">
+                                        See Details
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-
-
-const latestVehiclesPromise = fetch('http://localhost:3000/latest-vehicles').then(res => res.json());
-
-console.log(latestVehiclesPromise)
+import React, { useEffect, useState } from 'react';
+import Slider from '../../components/Slider';
 
 const Home = () => {
+    const [sliderVehicles, setSliderVehicles] = useState([]);
+
+    // fetch Slider Vehicle Data from MongoDB
+    useEffect(() => {
+        fetch("http://localhost:3000/latest-vehicles")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setSliderVehicles(data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+
     return (
         <div>
+            <Slider sliderVehicles={sliderVehicles}></Slider>
             This is Homepage
         </div>
     );
