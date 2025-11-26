632b694aac05af0953a654a0be2c2a72ba7b2ce7
naymulislamnay <naymulislamnay@gmail.com>
2025-11-26 16:57:22 +0600

    password show hide button added

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Footer from '../components/Footer';
 
 const MainLayout = () => {
+    const location = useLocation();
+    const isAuthPage = location.pathname === "/log-in" || location.pathname === "/sign-up";
+
     return (
         <div className="bg-[#e5d0ff] min-h-screen flex flex-col">
             <Navbar></Navbar>
-            <div className="max-w-[1200px] mx-auto w-full grow">
+            <div className={`max-w-[1200px] mx-auto w-full grow ${isAuthPage ? "flex-1 flex justify-center items-center" : ""}`}>
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
